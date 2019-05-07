b9f8446b2730e6b59a2671fca4efa5f9db9043ba
Jason Logan <jlogan@unitedfiregroup.com>
2019-05-07 16:35:25 -0500

    info add on

--- ui-kit/.storybook/config.js
+++ ui-kit/.storybook/config.js
@@ -1,4 +1,5 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from "@storybook/addon-info";
 
 //uses webpacks require.context https://webpack.js.org/guides/dependency-management/#require-context
 //directory to search, a flag indicating whether subdirectories should be searched too, and a regular expression to match files against
@@ -9,5 +10,11 @@ const loadStories = () => {
   req.keys().forEach(filename => req(filename));
 };
 
+//addon-info needs to be the first decorator or it gives info on previous decorator and not the component
+addDecorator(
+  withInfo({
+    inline: true
+  })
+);
 
 configure(loadStories, module);
