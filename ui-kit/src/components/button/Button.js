5dc5c630a47d0449d5c837029bef83cb71d3c0c0
Jason Logan <jlogan@unitedfiregroup.com>
2019-05-07 15:38:05 -0500

    refactored buttons for easier presentation

--- ui-kit/src/components/button/Button.js
+++ ui-kit/src/components/button/Button.js
@@ -1,6 +1,14 @@
 import React from "react";
 import propTypes from "prop-types";
-import { ButtonBase } from "./ButtonBase"
+import styled from "styled-components";
+
+const ButtonBase = styled.button`
+    height: 50px;
+    width: 200px;
+    color: white;
+    background-color: green;
+    border-radius: 5px;
+`
 
 const Button = () =>{
     return <ButtonBase>Submit</ButtonBase>
--- ui-kit/src/components/button/ButtonBase.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const ButtonBase = styled.button`
-    height: 50px;
-    width: 200px;
-    color: white;
-    background-color: green;
-    border-radius: 5px;
-`
