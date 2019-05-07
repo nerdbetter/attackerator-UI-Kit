5654eb10b6b5df4ee63c14dbb581320919e9f6d9
Jason Logan <jlogan@unitedfiregroup.com>
2019-05-07 16:07:23 -0500

    added some options to button

--- ui-kit/src/components/button/Button.stories.js
+++ ui-kit/src/components/button/Button.stories.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Button from "./Button"
+import Button from "./Button";
 
-storiesOf("Button", module)
-  .add("Basic Button", () =>{
-   return <Button />
-  })
+storiesOf("Button", module).add("Basic Button", () => {
+  return <Button color={"orange"} >Submit</Button>;
+});
