8d20f104d4933e564f05f9efd341f465daa38100
Jason Logan <jlogan@unitedfiregroup.com>
2019-05-08 10:12:24 -0500

    added knob vars to Button story

--- ui-kit/src/components/button/Button.stories.js
+++ ui-kit/src/components/button/Button.stories.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Button from "./Button";
+import { text, select } from "@storybook/addon-knobs";
+
+const colorOptions = {
+  green: "green",
+  lightBlue: "lightBlue",
+  purple: "purple",
+  orange: "orange",
+  grey: "grey",
+  red: "red"
+};
 
 storiesOf("Button", module).add("Basic Button", () => {
-  return <Button color={"orange"} >Submit</Button>;
+  const color = select("color", colorOptions);
+  const children = text("children", "submit");
+  return <Button color={color}>{children}</Button>;
 });
